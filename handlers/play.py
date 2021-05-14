b805fafe40e9f90abfd6d886ac482985fe8dc261
Ikyy <84012385+ikyy35@users.noreply.github.com>
2021-05-14 19:48:21 +0700

    Create play.py

--- handlers/play.py
+++ handlers/play.py
@@ -512,7 +512,7 @@ async def play(_, message: Message):
                 ],                     
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/remaja_virtual62"
+                        "Owner", url="https://t.me/boyfriendnice"
                     )
                 ],
                 [       
@@ -648,7 +648,7 @@ async def deezer(client: Client, message_: Message):
                 ],                     
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/remaja_virtual62"
+                        "Owner", url="https://t.me/boyfriendnice"
                     )
                 ],
                 [       
@@ -784,7 +784,7 @@ async def jiosaavn(client: Client, message_: Message):
                 ],                     
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/remaja_virtual62"
+                        "Owner", url="https://t.me/boyfriendnice"
                     )
                 ],
                 [       
